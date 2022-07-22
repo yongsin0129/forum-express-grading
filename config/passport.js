e1012bc3cd8cab3acfaf87d0bcf469a542bb0893
ysRedhouse <yongsin0129@gmail.com>
2022-07-22 11:28:04 +0800

    feat: add api-auth middleware

--- config/passport.js
+++ config/passport.js
@@ -1,27 +1,53 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const passportJWT = require('passport-jwt')
 const bcrypt = require('bcryptjs')
 const { User, Restaurant } = require('../models')
+
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+
 // set up Passport strategy
-passport.use(new LocalStrategy(
-  // customize user field
-  {
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-  },
-  // authenticate user
-  (req, email, password, cb) => {
-    User.findOne({ where: { email } })
-      .then(user => {
+passport.use(
+  new LocalStrategy(
+    // customize user field
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    // authenticate user
+    (req, email, password, cb) => {
+      User.findOne({ where: { email } }).then(user => {
         if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
         bcrypt.compare(password, user.password).then(res => {
           if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
           return cb(null, user)
         })
       })
-  }
-))
+    }
+  )
+)
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+}
+passport.use(
+  // /signin 時已經把使用者資料放進 payload，所以這裡在解開 token 之後，可以運用 jwtPayload.id 去 User table 裡找出使用者。
+  new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
+    User.findByPk(jwtPayload.id, {
+      include: [
+        { model: Restaurant, as: 'FavoritedRestaurants' },
+        { model: Restaurant, as: 'LikedRestaurants' },
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
+      ]
+    })
+      .then(user => cb(null, user))
+      .catch(err => cb(err))
+  })
+)
 // serialize and deserialize user
 passport.serializeUser((user, cb) => {
   cb(null, user.id)
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -3,14 +3,15 @@ const router = express.Router()
 const passport = require('../../config/passport')
 const admin = require('./modules/admin')
 const { apiErrorHandler } = require('../../middleware/error-handler')
+const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 const restController = require('../../controllers/apis/restaurant-controller')
 const userController = require('../../controllers/apis/user-controller')
 
-router.use('/admin', admin)
+router.use('/admin', authenticated, authenticatedAdmin, admin)
 
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', apiErrorHandler)
 
 module.exports = router
